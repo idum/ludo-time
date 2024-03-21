ab7f75ecf4a848ecbe1a70ff886da508e3151ed5
Ido Traini <ido.traini@gmail.com>
2024-03-21 15:26:57 +0100

    inserito seed e

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+evento1=Event.create!(title: "Evento ludico 1", location: "Via Pio Panfili 211, Porto San Giorgio", date: Date.today + 1.month)
+evento2=Event.create!(title: "Evento ludico 2", location: "Ascoli Piceno", date: Date.today + 2.month)
+evento3=Event.create!(title: "Evento ludico 3", location: "Ancona", date: "21-09-2026")
+
+User.create!(email:"me@me.me", password: "mememe", admin:true)
+User.create!(email:"a@a.a", password: "password")
